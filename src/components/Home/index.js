e4d3bff5e53d591905cb33c119c183a111e3cf95
Jake Zerrer <him@jakezerrer.com>
2016-09-16 22:30:03 -0400

    Add ui-router-react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,14 +4,16 @@ import styles from './styles.css';
 export default function Home() {
   return (
     <div className={styles.wrap}>
-      <h1 className={styles.name}>jake zerrer</h1>
-      <p className={styles.title}>designer / software engineer</p>
-      <ul className={styles.socialList}>
-        <li><a href="https://github.com/telekid">github<span className={styles.trail}>.com/telekid</span></a></li>
-        <li><a href="https://www.instagram.com/telekid/">instagram<span className={styles.trail}>.com/telekid</span></a></li>
-        <li><a href="https://keybase.io/hz">keybase<span className={styles.trail}>.io/hz</span></a></li>
-        <li><a href="https://www.linkedin.com/in/jakezerrer">linkedin<span className={styles.trail}>.com/u/jakezerrer</span></a></li>
-      </ul>
+      <div className={styles.box}>
+        <h1 className={styles.name}>jake zerrer</h1>
+        <p className={styles.title}>designer / software engineer</p>
+        <ul className={styles.socialList}>
+          <li><a href="https://github.com/telekid">github<span className={styles.trail}>.com/telekid</span></a></li>
+          <li><a href="https://www.instagram.com/telekid/">instagram<span className={styles.trail}>.com/telekid</span></a></li>
+          <li><a href="https://keybase.io/hz">keybase<span className={styles.trail}>.io/hz</span></a></li>
+          <li><a href="https://www.linkedin.com/in/jakezerrer">linkedin<span className={styles.trail}>.com/u/jakezerrer</span></a></li>
+        </ul>
+      </div>
     </div>
   );
 }
