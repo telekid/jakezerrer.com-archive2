526f47387ae4b07e0f0826ef66f869c12e999055
Jake Zerrer <him@jakezerrer.com>
2016-09-24 14:01:04 -0400

    Add reactcards

---
+++ src/reactcards.js
@@ -0,0 +1,9 @@
+import { run } from 'reactcards';
+import './reactcards/index.js';
+
+if (module.hot) {
+    module.hot.accept()
+}
+
+// off we go...
+run();
---
+++ src/reactcards/Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import cards from 'reactcards';
+
+const myCards = cards('Test');
+
+myCards.card(
+    `## Test Card
+  This is a test of reactcards`
+);
